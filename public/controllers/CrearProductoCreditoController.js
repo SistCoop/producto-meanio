adfa932fd3fbbe520b9972c051c48001f292abb5
carlosthe19916 <carlosthe19916@gmail.com>
2015-04-14 15:40:52 -0500

    CRED-30 crear producto ok, falta cargar los combos de tipo persona y tipo moneda

--- public/controllers/CrearProductoCreditoController.js
+++ public/controllers/CrearProductoCreditoController.js
@@ -1,18 +1,20 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.producto').controller('CrearProductoCreditoController', ['$scope', '$state', 'SGProductoCredito', 'SGTipoPersona',
-    function($scope, $state, SGProductoCredito, SGTipoPersona) {
+angular.module('mean.producto').controller('CrearProductoCreditoController', ['$scope', '$state', 'SGProductoCredito', 'SGTipoPersona', 'Notification',
+    function($scope, $state, SGProductoCredito, SGTipoPersona, Notification) {
 
         $scope.view = {
             producto:   SGProductoCredito.$build()
         };
 
         $scope.combo = {
-            tipoPersona: undefined
+            tipoPersona: undefined,
+            moneda: undefined
         };
         $scope.combo.selected = {
-            tipoPersona: undefined
+            tipoPersona: undefined,
+            moneda: undefined
         };
 
         $scope.loadCombo = function(){
@@ -20,5 +22,20 @@ angular.module('mean.producto').controller('CrearProductoCreditoController', ['$
         };
         $scope.loadCombo();
 
+        $scope.submit = function(){
+            if($scope.form.$valid){
+
+                $scope.view.producto.$save().then(
+                    function(response){
+                        Notification.success('Producto creado');
+                        $state.go('^.editarProductoCredito.resumen', {id: response.id});
+                    },
+                    function error(err){
+                        Notification.error(err.data.message);
+                    }
+                );
+
+            }
+        };
     }
 ]);
