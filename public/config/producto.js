b22e5c352cc1248a4a4212df45dd904de38fcffe
Carlos <carlosthe19916@gmail.com>
2015-04-29 13:03:43 -0500

    eliminar url

--- public/config/producto.js
+++ public/config/producto.js
@@ -2,18 +2,6 @@
 
 /* jshint -W098 */
 
-angular.module('mean.producto').config(function(sgProductoProvider) {
-    sgProductoProvider.restUrl = 'http://localhost:8080/producto/rest';
-});
-
-angular.module('mean.producto').config(function(sgIso4217Provider) {
-    sgIso4217Provider.restUrl = 'http://localhost:8080/iso4217/rest';
-});
-
-angular.module('mean.producto').config(function(sgPersonaProvider) {
-    sgPersonaProvider.restUrl = 'http://localhost:8080/persona/rest';
-});
-
 angular.module('mean.producto').controller('ProductoSidebarController', ['$scope', '$menuItemsProducto',
     function($scope, $menuItemsProducto) {
 
